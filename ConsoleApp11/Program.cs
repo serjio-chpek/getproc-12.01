1add5e5121488ba73afbf782fea58ad8c8e6d910
SerjP <brainditrff@gmail.com>
2023-01-12 17:25:03 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var process = Process.GetProcesses();
+        var calc = process.FirstOrDefault(x => x.ProcessName == "CalculatorApp");
+        foreach (var item in process)
+        {
+            Console.WriteLine($"{item.ProcessName}");
+        }
+        if (process.Any(x => x.ProcessName == "chrome"))
+        {
+            Console.WriteLine("на компе запущен хром");
+        }
+
+        if (calc != null)
+        {
+            calc.Kill();
+        }
+    }
+
+    private static int GetMbytser(int bytes) => bytes / 1048576;
+}
